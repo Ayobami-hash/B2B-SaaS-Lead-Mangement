a099a376cebc1515312ef2d7f99a431dfb08a008
AyobamiLad <moriahberry2222@gmail.com>
2024-06-17 07:29:23 +0100

    update

--- frontend/src/components/csv-upload.tsx
+++ frontend/src/components/csv-upload.tsx
@@ -36,7 +36,7 @@ const CSVUpload = () => {
 
   const handleConfirm = async () => {
     try {
-      const response = await axios.post('http://localhost:5000/api/upload-csv', {
+      const response = await axios.post('https://b2b-saas-lead-mangement-3.onrender.com/api/upload-csv', {
         csvData,
         fieldMappings,
       });
