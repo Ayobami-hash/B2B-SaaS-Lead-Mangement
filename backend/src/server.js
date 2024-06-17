b2b9d01fcc1071698febd71b1a14bac8d0dcb587
AyobamiLad <moriahberry2222@gmail.com>
2024-06-17 07:08:38 +0100

    update on CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,17 +16,21 @@ const app = express();
 
 
 // app.use(cors(corsOptions));
+var allowlist = ['https://b2-b-saa-s-lead-mangement.vercel.app', 'http://localhost:3000']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
 // Middleware
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors({
-  origin: 'http://localhost:3000', // or your frontend URL
-  credentials: true,
-}, {
-  origin: 'https://b2-b-saa-s-lead-mangement.vercel.app/', // or your frontend URL
-  credentials: true,
-}));
+app.use(cors(corsOptionsDelegate));
 app.use(cookieParser()); 
 
 // MongoDB connection
